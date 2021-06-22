1110b8b5fbf976a9af1339012d6ec459634d8261
“swwoosh” <1705149145@qq.com>
2021-06-22 16:29:36 +0800

    第八次提交

---
+++ Assets/Scripts/UItest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class UItest : MonoBehaviour
+{
+    public Slider mainvolumeSlider;
+    public AudioMixer mainMixer;
+
+    bool isPause = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainvolumeSlider.onValueChanged.AddListener(VolumeChange );
+    }
+
+    public void VolumeChange(float volume)
+    {
+        mainMixer.SetFloat("MainVolume", volume);
+    }
+    public void PauseGame()
+    { 
+        isPause = !isPause;
+        if(isPause )
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+       
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
